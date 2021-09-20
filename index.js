1e8a51595e9c56fafcbefb3fde0ae97fa13720fd
Jessica Nelson <yahwehschica@yahoo.com>
2021-09-20 15:28:18 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const body = document.getElementsByTagName("body")[0];
+body.removeChild(body.querySelector("main"))
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Jessica is the champion!"
+
+
